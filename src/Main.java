980a4dcb0cff90af9ef6f13d74e2c219a5c6831d
김형수 <senbonnobara@naver.com>
2026-03-17 15:40:51 +0900

    보석도둑 시간관리

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		long sum = 0;
-		int x = Integer.MAX_VALUE;
-		for(int i=0;i<3;i++) sum+=x;
-		System.out.println(sum);
+public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	int n = Integer.parseInt(st.nextToken());
+	int k = Integer.parseInt(st.nextToken());
+	List<int[]> jewel = new ArrayList<>(); 
+	Queue<Integer> bag = new PriorityQueue<>();
+	for(int i=0;i<n;i++) {
+		st = new StringTokenizer(br.readLine());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		jewel.add(new int[] {x,y});
+	}
+	Collections.sort(jewel, (a,b)->{
+		if(a[1]!=b[1]) return Integer.compare(b[1], a[1]);
+		else return Integer.compare(a[0], b[0]);
+	});
+	for(int i=0;i<k;i++) bag.add(Integer.parseInt(br.readLine()));
+	int ans = 0;
+	while(!bag.isEmpty()) {
+		int c = bag.poll();
+		int x = jewel.size();
+		for(int i=0;i<x;i++) {
+			if(jewel.get(i)[0]<=c) {
+				ans+=jewel.get(i)[1];
+				jewel.remove(i);
+				break;
+			}
+		}
+	}
+	System.out.println(ans);
 	}
 }
