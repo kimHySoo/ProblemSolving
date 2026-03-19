337671afd2b8966f5817ee9ee37fa9aa7a1c723b
KimHyoungSoo <senbonnobara@naver.com>
2026-03-19 22:08:17 +0900

    p23534 실버 2인데 못풀겠음 ㅇㅇ

---
+++ src/BOJ/p23435.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+        long ans = 1;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] > 0) {
+                ans *= (cnt[i] + 1);
+            }
+        }
+        System.out.println(ans - 1);
+    }
+}
+
